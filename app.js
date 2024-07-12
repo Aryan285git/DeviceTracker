d1d016745ec548dce78f4640d738499b1d91755b
Aryan Singh <aryansingh3418@gmail.com>
2024-07-12 21:35:50 +0530

    final

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const socketio= require('socket.io');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
 
-const server=https.createServer(app);
+// Use http.createServer instead of https.createServer
+const server = http.createServer(app);
 const io = socketio(server);
 
 app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public"))); // Corrected line
 
-io.on("connection",function(socket){
-    socket.on("send-location", function(data){
-        io.emit("receive-location",{id: socket.id, ...data});
+io.on("connection", function(socket) {
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", { id: socket.id, ...data });
     });
-    socket.on("disconnect", function(){
+    socket.on("disconnect", function() {
         io.emit("user-disconnected", socket.id);
-    }")
+    });
 });
 
-app.get("/",function(req,res){
-    res.render(index);
+app.get("/", function(req, res) {
+    res.render("index");
 });
 
-server.listen(3000,function(){
-    console.log("Server started on port 3000");
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,41 @@
-const socket=io();
+const socket = io();
 
-if(navigator.geoloaction){
-    navigator.geolocation.watchPosition((position)=>{
-        const{latitude,longitude}=position.coords;
-        socket.emit("send-location",{latitude,longitude});
-        socket.emit("send-location",{
-            latitude,
-            longitude
-    }},(error)=>{
-        console.error(error)
-    },
-    {
-       enableHighAccuracy:true,
-       timeout:5000
-       maximumAge:0,
-    }
-    );
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    socket.emit("send-location", { latitude, longitude });
+  }, (error) => {
+    console.error(error);
+    // You can add user-friendly error messages here (e.g., alert)
+  }, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
 }
-L.map("map").setView([0,0],10);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:'Aryan Singh',
-}
-).addTo(map)
+const map = L.map("map").setView([0, 0], 10); // Declare map here
 
-const markers={};
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>' // Correct attribution
+}).addTo(map);
 
-socket.on("receive-location", (data)=>{
-    const{id, latitude, longitude}=data;
-    map.setView([latitude, longitude], 16);
-    if(markers[id]){
-        markers[id].setLatLng([latitude, longitude]);
-    }else{
-        const marker=L.marker([latitude, longitude]).addTo(map);
-        markers[id]=marker;
-    }
+const markers = {};
+
+socket.on("receive-location", (data) => { // Pass map as argument
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 16); // Use passed map object
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    const marker = L.marker([latitude, longitude]).addTo(map);
+    markers[id] = marker;
+  }
 });
 
-socket.on("user-disconnected", (userId)=>{
-    if(markers[userId]){
-        map.removeLayer(markers[userId]);
-        delete markers[userId];
-    }
+socket.on("user-disconnected", (userId) => {
+  if (markers[userId]) {
+    map.removeLayer(markers[userId]);
+    delete markers[userId];
+  }
 });
